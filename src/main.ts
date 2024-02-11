a83654c245119c843731b96276791c90492e8972
bigbabyoftel <augustus.tb@outlook.com>
2024-02-11 19:03:06 +0200

    styling added

--- src/main.ts
+++
@@ -1,5 +0,0 @@
-const addTask = document.getElementById("add-to-list");
-const displayTask = document.getElementById("show-list");
-const taskDisplay = document.getElementById('taskDisplay');
-const otherTaskLists = document.getElementById("other-lists");
-
