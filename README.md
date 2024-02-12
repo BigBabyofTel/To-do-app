98dfb82e3bab0a74dd6d872906f06bebb374a4b1
bigbabyoftel <augustus.tb@outlook.com>
2024-02-12 06:37:30 +0200

    styling complete

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # To do app
 
 ## version 1.1 with TypeScript and TailwindCSS
+
+### Styling complete. Event listener needed for inputs and button
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+const taskInput = document.getElementById('taskInput') as HTMLElement;
+const taskDetails = document.getElementById('task-details') as HTMLElement;
+
+function addTask () {
+    if(taskInput === 'string') {
+        alert("please enter a task")
+    }
+}
